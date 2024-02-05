a4510e3b361c250abf27b3992998b6145a380470
Arjit40 <157403346+Arjit40@users.noreply.github.com>
2024-02-05 11:33:49 +0530

    Add files via upload

---
+++ user.m
@@ -0,0 +1,20 @@
+%Arjit Lodhi
+%2022BTECE002
+%LOGIN PORTAL
+
+clc;
+clear all;
+
+user='2022BTECE001';
+pwd='curaj123';
+
+
+username=input("Enter user id: ",'s');
+password=input("Enter password: ",'s');
+
+ if username == user
+            display("login succesfully");
+            else
+           display("incoorect usernme/password");
+        end;
+
